2398cebb38229be06ed61ab686cab3c54e6e6d7a
JerRamMissle <114175041+JerRamMissle@users.noreply.github.com>
2024-08-05 19:27:49 -0400

    Updated Portfolio 2

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+function Project() {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+                <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+                    Projects
+                </h1>
+            </div>
+        </>
+    );
+}
+export default Project;
